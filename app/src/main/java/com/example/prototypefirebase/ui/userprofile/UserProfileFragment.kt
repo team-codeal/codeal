20256ce56b360b289903bf2b442c16e4f9c0cbcf
Roman <42515597+ghB111@users.noreply.github.com>
2020-11-30 23:15:10 +0700

    Add basic UI to user profile

--- app/src/main/java/com/example/prototypefirebase/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/userprofile/UserProfileFragment.kt
@@ -4,28 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.prototypefirebase.R
 
 class UserProfileFragment : Fragment() {
 
-    private lateinit var homeViewModel: UserProfileViewModel
+    private lateinit var userProfileViewModel: UserProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
+
+        userProfileViewModel =
             ViewModelProvider(this).get(UserProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_userprofile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_userprofile)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        return inflater.inflate(R.layout.fragment_userprofile, container, false)
     }
 }
