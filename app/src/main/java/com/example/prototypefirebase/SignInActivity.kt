9bbc18bd5bec1b8356e3cdca7908318f072b459a
Roman <b111forjob@gmail.com>
2020-12-06 11:21:39 +0700

    Port existing windows to use CodealUser && features

--- app/src/main/java/com/example/prototypefirebase/SignInActivity.kt
+++ app/src/main/java/com/example/prototypefirebase/SignInActivity.kt
@@ -7,79 +7,59 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
+import com.example.prototypefirebase.codeal.CodealUser
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
 
     private val RC_SIGN_IN = 36
-    lateinit var statusBar : TextView
+    private var user: CodealUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            user = CodealUser()
+            logInSuccessful()
+        } catch (_: IllegalStateException) {}
+
         setContentView(R.layout.activity_signin)
+
         val loginButton : ImageButton = findViewById(R.id.button_log_in)
-//        loginButton.setOnClickListener(View.OnClickListener() {
-//            fun onClick(view: View) = logIn(view)
-//        })
         loginButton.setOnClickListener {
             val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
             startActivityForResult(
                 AuthUI.getInstance().createSignInIntentBuilder()
                     .setAvailableProviders(providers).build(), RC_SIGN_IN)
         }
-        val logoutButton : ImageButton = findViewById(R.id.button_log_out)
-        logoutButton.setOnClickListener {
-            AuthUI.getInstance().signOut(this).addOnCompleteListener{
-                this.statusBar.text = "SIGNED OUT"
-            }
-        }
 
-        val statusBar : TextView = findViewById(R.id.text_status)
-        this.statusBar = statusBar
+    }
 
-        val user = FirebaseAuth.getInstance().currentUser
-        if (user != null) {
-            this.statusBar.text = "User " + user.displayName + " is signed in"
-        } else {
-            this.statusBar.text = "Please, sign in"
-        }
-
-        val proceedButton : Button = findViewById(R.id.button_procceed)
-
-        proceedButton.setOnClickListener {
-            val user = FirebaseAuth.getInstance().currentUser
-            if (user == null) {
-                this.statusBar.text = "Sorry, first you have to sign in"
-            } else {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-//                val navController = this.findNavController(R.id.nav_host_fragment)
-//                navController.navigate(R.id.action_mainActivity_to_databaseActivity)
-            }
-        }
-
+    private fun logInSuccessful() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            //val response = IdpResponse.fromResultIntent(data)
 
             if (resultCode == Activity.RESULT_OK) {
                 // signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user == null) {
-                    this.statusBar.text = "OK SOMETHING WEIRD HAPPENED HERE\n Your user object is null wtf"
-                } else {
-                    this.statusBar.text = "User " + user.displayName + " is signed in"
+                try {
+                    user = CodealUser {
+                        Toast.makeText(this, "Hello ${user?.name}!", Toast.LENGTH_SHORT).show()
+                    }
+                    logInSuccessful()
+                } catch (_: java.lang.IllegalStateException) {
+                    Toast.makeText(this, "Weird error during signing in...", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                this.statusBar.text = "AUTH FAILED"
+                Toast.makeText(this, "Error occurred during signing in", Toast.LENGTH_SHORT).show()
             }
-        } else {
-            this.statusBar.text = "Received some strange requestCode"
         }
     }
 }
--- app/src/main/java/com/example/prototypefirebase/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/userprofile/UserProfileFragment.kt
@@ -1,16 +1,19 @@
 package com.example.prototypefirebase.ui.userprofile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.prototypefirebase.R
+import com.example.prototypefirebase.SignInActivity
 import com.example.prototypefirebase.codeal.CodealUser
-import com.google.firebase.firestore.FirebaseFirestore
+import com.firebase.ui.auth.AuthUI
 
 class UserProfileFragment : Fragment() {
 
@@ -40,11 +43,22 @@ class UserProfileFragment : Fragment() {
 
         updateUserProfileUI()
 
-        val saveButton: Button? = view.findViewById(R.id.button_send_to_db)
+        val saveButton: Button = view.findViewById(R.id.button_send_to_db)
 
-        saveButton?.setOnClickListener {
+        saveButton.setOnClickListener {
             saveCurrentUserProfile()
             // updateUserProfileUI()
+        }
+
+        val ctx = requireContext()
+
+        val logoutButton : Button = view.findViewById(R.id.button_log_out)
+        logoutButton.setOnClickListener {
+            AuthUI.getInstance().signOut(ctx).addOnCompleteListener{
+                Toast.makeText(ctx, "Signed out", Toast.LENGTH_SHORT).show()
+                val intent = Intent(ctx, SignInActivity::class.java)
+                startActivity(intent)
+            }
         }
 
     }
