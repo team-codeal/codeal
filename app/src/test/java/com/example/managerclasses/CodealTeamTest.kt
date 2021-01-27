98e87451e0070fef3cb46ca5705589bf6c561f72
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-01-27 16:25:46 +0700

    Create CodealTeamTest.kt

---
+++ app/src/test/java/com/example/managerclasses/CodealTeamTest.kt
@@ -0,0 +1,12 @@
+package com.example.managerclasses
+import org.junit.Test
+
+import org.junit.Assert.*
+class CodealTeamTest {
+    @Test fun test1() {
+        var team = CodealTeam("zalupa", emptyList())
+        //team.addTeamToDatabase();
+        assertEquals(4,2+2)
+    }
+
+}
