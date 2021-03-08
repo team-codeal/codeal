20b97eef38fa575288f1239e9148e0f85307bca3
Roman <42515597+ghB111@users.noreply.github.com>
2021-03-08 23:36:51 +0700

    Delete useless TeamsViewModel

--- app/src/main/java/com/example/prototypefirebase/ui/teams/TeamsFragment.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/teams/TeamsFragment.kt
@@ -5,31 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.prototypefirebase.R
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_teams.*
 import kotlinx.android.synthetic.main.fragment_teams.view.*
 
 class TeamsFragment : Fragment(), OnTeamClickListener {
 
     private var teams = ArrayList<Model>()
-    private lateinit var notificationsViewModel: TeamsViewModel
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(TeamsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_teams, container, false)
 
         getTeams()
