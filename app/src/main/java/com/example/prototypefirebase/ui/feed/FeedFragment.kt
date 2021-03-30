0c6cd63dc6b7ee9ff3c07cec44434df51e2d9da4
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-03-30 13:29:26 +0700

    fixed_bug_feed

--- app/src/main/java/com/example/prototypefirebase/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/feed/FeedFragment.kt
@@ -30,6 +30,7 @@ class FeedFragment : Fragment(), OnTaskClickListener {
         dashboardViewModel =
             ViewModelProvider(this).get(FeedViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_feed, container, false)
+
         getTasks()
         /*val textView: TextView = root.findViewById(R.id.text_feed)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -39,18 +40,19 @@ class FeedFragment : Fragment(), OnTaskClickListener {
     }
 
     private fun getTasks() {
+
+        tasks.clear()
+
         val db = FirebaseFirestore.getInstance()
         db.collection("tasks1")
             .get()
             .addOnSuccessListener { result ->
-                var i = 0
                 for (document in result) {
                     val task = Task(
                         document.data["FirebaseID"] as String,
                         document.data["Name"] as String,
                         document.data["Text"] as String
                     )
-                    i++
                     tasks.add(task)
                 }
 
