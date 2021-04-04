c4038819ce53c694fc2f20de4d2c1dff43dfdd1a
Roman <b111forjob@gmail.com>
2021-04-04 23:51:27 +0700

    Fix Feed for CodealTask

--- app/src/main/java/com/example/prototypefirebase/ui/feed/FeedTasksAdapter.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/feed/FeedTasksAdapter.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prototypefirebase.R
-import com.example.prototypefirebase.Task
+import com.example.prototypefirebase.codeal.CodealTask
 import com.example.utils.recyclers.tasks.OnTaskClickListener
 import kotlinx.android.synthetic.main.layout_list_item.view.*
 import java.util.ArrayList
 
 class FeedTasksAdapter(
-    private val tasks: ArrayList<Task>,
+    private val tasks: ArrayList<CodealTask>,
     private val onTaskClickListener: OnTaskClickListener
 ) : RecyclerView.Adapter<FeedTasksViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedTasksViewHolder {
