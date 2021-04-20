57d9e541b2bb777b574349c2c098a6ae4cccaf40
Roman <b111forjob@gmail.com>
2021-04-20 15:17:16 +0700

    AddTaskActivity optimise imports

--- app/src/main/java/com/example/prototypefirebase/AddTaskActivity.kt
+++ app/src/main/java/com/example/prototypefirebase/AddTaskActivity.kt
@@ -1,13 +1,10 @@
 package com.example.prototypefirebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.prototypefirebase.codeal.CodealTask
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.FirebaseFirestore
 
 class AddTaskActivity : AppCompatActivity() {
 
