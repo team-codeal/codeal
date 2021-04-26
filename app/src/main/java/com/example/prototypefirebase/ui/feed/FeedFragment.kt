982f23f36e088294e493e99a9ce3632ee6ef6e28
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-04-27 00:22:44 +0700

    feed_adding_non_global_tasks_changed_adapter_feed_fragment

--- app/src/main/java/com/example/prototypefirebase/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/example/prototypefirebase/ui/feed/FeedFragment.kt
@@ -1,15 +1,18 @@
 package com.example.prototypefirebase.ui.feed
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prototypefirebase.R
+import com.example.prototypefirebase.ViewTaskDetailActivity
 import com.example.prototypefirebase.codeal.CodealTask
 import com.example.utils.recyclers.tasks.OnTaskClickListener
 import com.example.utils.recyclers.tasks.TaskAdapter
@@ -18,8 +21,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 class FeedFragment : Fragment(), OnTaskClickListener {
 
     private lateinit var dashboardViewModel: FeedViewModel
-    private var tasks = ArrayList<CodealTask>()
-    private var taskAdapter: FeedTasksAdapter = FeedTasksAdapter(tasks, this)
+    private var tasks: MutableList<String> = mutableListOf()
+    private lateinit var taskAdapter: TaskAdapter
 
 
     override fun onCreateView(
@@ -40,7 +43,11 @@ class FeedFragment : Fragment(), OnTaskClickListener {
         super.onViewCreated(view, savedInstanceState)
         val feedRecyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_feed)!!
 
-        taskAdapter = FeedTasksAdapter(tasks, this)
+        taskAdapter = TaskAdapter(tasks){
+            val intent = Intent(context, ViewTaskDetailActivity::class.java)
+            intent.putExtra("TaskID", tasks[it])
+            startActivity(intent)
+        }
         feedRecyclerView.adapter = taskAdapter
         feedRecyclerView.layoutManager = LinearLayoutManager(activity)
     }
@@ -55,7 +62,7 @@ class FeedFragment : Fragment(), OnTaskClickListener {
             .addOnSuccessListener { result ->
                 for (document in result) {
                     val task = CodealTask(document.id) { taskAdapter.notifyDataSetChanged() }
-                    tasks.add(task)
+                    tasks.add(task.id)
                 }
 
             }
--- app/src/main/java/com/example/prototypefirebase/ui/feed/FeedTasksViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.prototypefirebase.ui.feed
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class FeedTasksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-}
