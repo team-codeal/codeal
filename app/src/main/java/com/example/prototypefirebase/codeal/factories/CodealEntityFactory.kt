c1c2ab28e72b652bd72fae49f3048446bd6e2de8
Roman <b111forjob@gmail.com>
2021-05-06 17:07:31 +0700

    CodealEntityFactory add documentation

--- app/src/main/java/com/example/prototypefirebase/codeal/factories/CodealEntityFactory.kt
+++ app/src/main/java/com/example/prototypefirebase/codeal/factories/CodealEntityFactory.kt
@@ -4,6 +4,11 @@ import com.example.prototypefirebase.codeal.CodealEntity
 
 interface CodealEntityFactory<T: CodealEntity<T>> {
 
+    /**
+     * Get a CodealEntity object by it's id (or some identification).
+     * @param id id or url or generally some identification for a CodealEntity object from Firestore
+     * @return CodealEntity object that of that id.
+     */
     fun get(id: String): T
 
 }
