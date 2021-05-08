a5ddc6cfd6548e78ac32f39217df8794a94d513b
Roman <b111forjob@gmail.com>
2021-05-08 19:18:22 +0700

    ListViewHolder + ListAdapter refactor

--- app/src/main/java/com/example/utils/recyclers/lists/ListViewHolder.kt
+++ app/src/main/java/com/example/utils/recyclers/lists/ListViewHolder.kt
@@ -1,8 +1,25 @@
 package com.example.utils.recyclers.lists
 
+import android.content.Context
 import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.prototypefirebase.R
 
-class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ListViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
+
+    private val listNameHolder : TextView = itemView.findViewById(R.id.list_name)
+    val tasksRecyclerView: RecyclerView = itemView.findViewById(R.id.item_item_list)
+
+    var label = ""
+        set(value) {
+            field = value
+            listNameHolder.text = value
+        }
+
+    init {
+        tasksRecyclerView.layoutManager = LinearLayoutManager(context)
+    }
 
 }
