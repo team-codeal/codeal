65d81275bf55d2049c0100229d6f6f0dc04c6b33
Roman <b111forjob@gmail.com>
2021-05-15 22:07:27 +0700

    Support simple dragging in task lists

--- app/src/main/java/com/example/utils/recyclers/tasks/TaskAdapter.kt
+++ app/src/main/java/com/example/utils/recyclers/tasks/TaskAdapter.kt
@@ -2,6 +2,8 @@ package com.example.utils.recyclers.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prototypefirebase.R
 
@@ -9,6 +11,40 @@ class TaskAdapter(
     private val taskIDs: MutableList<String>,
     private val onTaskClickListenerCallback: ((Int) -> Unit)
 ) : RecyclerView.Adapter<TaskViewHolder>() {
+
+    inner class TaskItemTouchHelperCallback: ItemTouchHelper.Callback() {
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            return makeMovementFlags(UP or DOWN, LEFT or RIGHT)
+        }
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            changeTaskIDsPositions(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            TODO("Implement moving tasks between lists")
+        }
+    }
+
+    private fun changeTaskIDsPositions(holderPosition: Int, targetPosition: Int) {
+        val taskID = taskIDs.removeAt(holderPosition)
+        taskIDs.add(targetPosition, taskID)
+        notifyItemMoved(holderPosition, targetPosition)
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        (ItemTouchHelper(TaskItemTouchHelperCallback())).attachToRecyclerView(recyclerView)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         return TaskViewHolder(
             LayoutInflater.from(parent.context).inflate(
