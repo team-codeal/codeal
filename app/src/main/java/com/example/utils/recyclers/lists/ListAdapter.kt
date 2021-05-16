17f2c7b3533fd25184d04b60f4aaa9d34d7829af
Roman <b111forjob@gmail.com>
2021-05-16 22:52:03 +0700

    Enhance board recycler view
    Поменял list_list_item: вложил cardView в cardView, внешний сделал прозрачным.

--- app/src/main/java/com/example/utils/recyclers/lists/ListAdapter.kt
+++ app/src/main/java/com/example/utils/recyclers/lists/ListAdapter.kt
@@ -2,13 +2,16 @@ package com.example.utils.recyclers.lists
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Point
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prototypefirebase.R
 import com.example.prototypefirebase.ViewTaskDetailActivity
 import com.example.utils.recyclers.tasks.TaskAdapter
+
 
 class ListAdapter(
     private val listNames: MutableList<String>,
@@ -19,11 +22,23 @@ class ListAdapter(
     private var taskAdapters: MutableMap<String, TaskAdapter> = HashMap()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        return ListViewHolder(
+        val listViewHolder = ListViewHolder(
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.list_list_item, parent, false),
             context
         )
+        val itemView = listViewHolder.itemView
+
+        itemView.layoutParams.height = (getScreenWidth() * 1.4).toInt()
+        return listViewHolder
+    }
+
+    private fun getScreenWidth(): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = wm.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        return size.x
     }
 
     override fun getItemCount(): Int {
@@ -38,8 +53,10 @@ class ListAdapter(
 
         holder.tasksRecyclerView.adapter = TaskAdapter(listNameToTasksList[list]!!) {
             val intent = Intent(context, ViewTaskDetailActivity::class.java)
-            intent.putExtra("TaskID", listNameToTasksList[list]
-                ?.get(it))
+            intent.putExtra(
+                "TaskID", listNameToTasksList[list]
+                    ?.get(it)
+            )
             startActivity(context, intent, null)
         }
         taskAdapters[list] = holder.tasksRecyclerView.adapter as TaskAdapter
