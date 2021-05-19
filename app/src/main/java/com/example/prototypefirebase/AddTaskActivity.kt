9b6602e7b9cd6bd36768c5e3939243ea802394da
Roman <b111forjob@gmail.com>
2021-05-19 20:03:33 +0700

    Redo task adding

--- app/src/main/java/com/example/prototypefirebase/AddTaskActivity.kt
+++ app/src/main/java/com/example/prototypefirebase/AddTaskActivity.kt
@@ -1,10 +1,12 @@
 package com.example.prototypefirebase
 
 import android.os.Bundle
+import android.view.View.GONE
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.prototypefirebase.codeal.factories.CodealTaskFactory
 
 
@@ -18,12 +20,21 @@ class AddTaskActivity : AppCompatActivity() {
         teamID = intent.getStringExtra("TeamID")!!
         taskListName = intent.getStringExtra("TaskList")!!
 
-        setContentView(R.layout.activity_create_task)
+        setContentView(R.layout.activity_task_detail)
         supportActionBar?.hide();
 
-        val saveTaskButton: Button = findViewById(R.id.saveTask)
+        val saveTaskButton: Button = findViewById(R.id.updateTask)
         val taskNameHolder: TextView = findViewById(R.id.Name_task)
         val taskTextHolder: TextView = findViewById(R.id.Text_task)
+
+        val listNameHolder: TextView = findViewById(R.id.List_task)
+        listNameHolder.text = taskListName
+
+        val newCommentContainer: ConstraintLayout = findViewById(R.id.new_comment_container)
+        newCommentContainer.visibility = GONE
+
+        val deleteButton: Button = findViewById(R.id.deleteTask)
+        deleteButton.visibility = GONE
 
         saveTaskButton.setOnClickListener {
             val taskName = taskNameHolder.text.toString()
