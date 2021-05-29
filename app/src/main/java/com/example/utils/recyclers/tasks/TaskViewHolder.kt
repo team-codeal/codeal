b36425114122a3f2126999f66ec4f91e08814143
Roman <b111forjob@gmail.com>
2021-05-29 18:39:20 +0700

    Add support for displaying "tasks on fire"

--- app/src/main/java/com/example/utils/recyclers/tasks/TaskViewHolder.kt
+++ app/src/main/java/com/example/utils/recyclers/tasks/TaskViewHolder.kt
@@ -1,19 +1,21 @@
 package com.example.utils.recyclers.tasks
 
 import android.view.View
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prototypefirebase.R
 import com.example.prototypefirebase.codeal.CodealEntity
 import com.example.prototypefirebase.codeal.CodealTask
 import com.example.prototypefirebase.codeal.factories.CodealTaskFactory
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.*
 
 class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val taskNameHolder: TextView = itemView.findViewById(R.id.task_name)
     private val taskContentHolder: TextView = itemView.findViewById(R.id.task_content)
     private val taskCommentCountHolder: TextView = itemView.findViewById(R.id.comment_count)
+    private val taskHolder: RelativeLayout = itemView.findViewById(R.id.task_holder)
 
     var taskID: String? = null
 
@@ -25,6 +27,22 @@ class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 taskCommentCountHolder.text = task.commentsIDs.size.toString()
                 taskContentHolder.text = task.content
                 taskNameHolder.text = task.name
+
+                val deadline = task.deadline
+                if (deadline != null) {
+                    val now = Calendar.getInstance()
+                    val deadlineCalendar = Calendar.getInstance().apply { time = deadline }
+                    val deadLineIsTodayOrBefore =
+                        now.get(Calendar.DAY_OF_YEAR) >= deadlineCalendar.get(Calendar.DAY_OF_YEAR) &&
+                                now.get(Calendar.YEAR) >= deadlineCalendar.get(Calendar.YEAR)
+                    if (deadLineIsTodayOrBefore) {
+                        taskHolder.setBackgroundResource(R.drawable.deadline_task_item)
+                    } else {
+                        taskHolder.setBackgroundResource(0)
+                    }
+                } else {
+                    taskHolder.setBackgroundResource(0)
+                }
             }
         }
     }
