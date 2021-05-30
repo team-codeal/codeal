cdbd3f9c2ebb8bfe6a88d5207d89e77f2569b7d3
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-05-31 00:10:14 +0700

    task_emotions_add_tasks_likes

--- app/src/main/java/com/example/prototypefirebase/codeal/CodealTask.kt
+++ app/src/main/java/com/example/prototypefirebase/codeal/CodealTask.kt
@@ -1,15 +1,17 @@
 package com.example.prototypefirebase.codeal
 
+import com.example.prototypefirebase.codeal.factories.CodealEmotionFactory
 import com.example.prototypefirebase.codeal.factories.CodealTeamFactory
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 //TODO This Task class is for the global team "ultimate team". change later, delete the todo
 
-class CodealTask : CodealEntity<CodealTask> {
+class CodealTask : CodealEntity<CodealTask>, Likeable<CodealTask> {
 
     var name: String = ""
         private set
@@ -19,7 +21,11 @@ class CodealTask : CodealEntity<CodealTask> {
         private set
     var teamID: String = ""
         private set
+    var ownerID: String = ""
+        private set
     var commentsIDs: List<String> = emptyList()
+        private set
+    override var emotions: List<String> = emptyList()
         private set
 
     companion object {
@@ -29,6 +35,7 @@ class CodealTask : CodealEntity<CodealTask> {
         private const val TASKS_DB_TASK_LIST: String = "list"
         private const val TASKS_DB_TEAM_ID: String = "teamID"
         private const val TASKS_DB_COMMENTS_IDs: String = "comments_ids"
+        private const val TASKS_DB_EMOTIONS_IDS_FIELD_NAME: String = "emotions"
     }
 
     // constructor for an existing task
@@ -38,11 +45,13 @@ class CodealTask : CodealEntity<CodealTask> {
 
     // constructor for a new
     constructor(
-        taskName: String, taskContent: String, teamID: String, listName: String) {
+        taskName: String, taskContent: String, teamID: String, listName: String, ownerID: String) {
         name = taskName
         content = taskContent
         this.teamID = teamID
         this.listName = listName
+        this.ownerID = ownerID
+        this.emotions = emptyList()
         uploadTaskInfoToDB()
     }
 
@@ -136,6 +145,45 @@ class CodealTask : CodealEntity<CodealTask> {
             tasksDB.document(id).update(TASKS_DB_COMMENTS_IDs, newComments)
             newComments
         }
+        emotions = (snapshot.get(TASKS_DB_EMOTIONS_IDS_FIELD_NAME)
+                as? List<*>)?.filterIsInstance<String>() ?:
+                run {
+                    val newEmotionsList = emptyList<String>()
+                    tasksDB.document(id).update(TASKS_DB_EMOTIONS_IDS_FIELD_NAME,
+                        newEmotionsList)
+                    newEmotionsList
+                }
+    }
+
+    override fun likeBy(userID: String) {
+        CodealEmotionFactory.create(userID, id).addOnReady { emotion ->
+            val tasksDB = getDB()
+            tasksDB.document(id).update(TASKS_DB_EMOTIONS_IDS_FIELD_NAME,
+                FieldValue.arrayUnion(emotion.id))
+        }
+    }
+
+    override fun removeLikeBy(userID: String) {
+        val emotionsDB = FirebaseFirestore.getInstance()
+            .collection(CodealEmotion.EMOTIONS_DB_COLLECTION_NAME)
+
+        emotionsDB
+            .whereEqualTo(CodealEmotion.EMOTIONS_DB_PARENT_OBJECT_ID_FIELD_NAME, id)
+            .whereEqualTo(CodealEmotion.EMOTIONS_DB_OWNER_ID_FIELD_NAME, userID)
+            .get().addOnSuccessListener { queryResult ->
+
+                if (queryResult.isEmpty) return@addOnSuccessListener
+
+                for (emotionDocument in queryResult.documents) {
+                    val emotionID = emotionDocument.id
+                    CodealEmotionFactory.get(emotionID).addOnReady { emotion ->
+                        val commentsDB = getDB()
+                        commentsDB.document(id).update(TASKS_DB_EMOTIONS_IDS_FIELD_NAME,
+                            FieldValue.arrayRemove(emotion.id))
+                        emotion.delete()
+                    }
+                }
+            }
     }
 
 }
