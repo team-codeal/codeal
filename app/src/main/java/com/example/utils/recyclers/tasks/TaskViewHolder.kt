382d03f0250e16936600f47b1554943269c27a42
Roman <b111forjob@gmail.com>
2021-05-31 14:25:58 +0700

    Deadline logic: light up 2 days before

--- app/src/main/java/com/example/utils/recyclers/tasks/TaskViewHolder.kt
+++ app/src/main/java/com/example/utils/recyclers/tasks/TaskViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.utils.recyclers.tasks
 
+import android.icu.text.DateFormat.DAY
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -30,11 +31,14 @@ class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
                 val deadline = task.deadline
                 if (deadline != null) {
-                    val now = Calendar.getInstance()
+                    val maxDaysTillDeadline = 2
+                    val now = Calendar.getInstance().apply {
+                        add(Calendar.DATE, maxDaysTillDeadline)
+                    }
                     val deadlineCalendar = Calendar.getInstance().apply { time = deadline }
                     val deadLineIsTodayOrBefore =
-                        now.get(Calendar.DAY_OF_YEAR) >= deadlineCalendar.get(Calendar.DAY_OF_YEAR) &&
-                                now.get(Calendar.YEAR) >= deadlineCalendar.get(Calendar.YEAR)
+                        now.get(Calendar.DAY_OF_YEAR) >= deadlineCalendar.get(Calendar.DAY_OF_YEAR)
+                                && now.get(Calendar.YEAR) >= deadlineCalendar.get(Calendar.YEAR)
                     if (deadLineIsTodayOrBefore) {
                         taskHolder.setBackgroundResource(R.drawable.deadline_task_item)
                     } else {
